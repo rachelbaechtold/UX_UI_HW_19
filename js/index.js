94757e8709bce747a523ba0969917cf90a36a34d
rachelbaechtold <rachel.baechtold@du.edu>
2024-05-08 15:47:41 -0600

    Tutoring Session Commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 console.log("Your index.js file is loaded correctly!");
 
-$(document).ready(function(){
-    $('button').on("click", function(){
-        alert("Thanks for your interest!");
-    });
-});
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
