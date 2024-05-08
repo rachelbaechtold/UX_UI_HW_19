5a4977fe15b5bdd82e8ad2a3380f0cafabc29cfa
rachelbaechtold <rachel.baechtold@du.edu>
2024-05-08 15:05:42 -0600

    05.08.2024 Commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function(){
+    $('button').on("click", function(){
+        alert("Thanks for your interest!");
+    });
+});
